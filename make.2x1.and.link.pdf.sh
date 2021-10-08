a64b74709551bb654e5057b7d7446192939032e6
Michael Gilchrist <mikeg@utk.edu>
2021-10-08 10:46:41 -0400

    updated make.2x1.sh to work with files in /tmp

--- make.2x1.and.link.pdf.sh
+++ make.2x1.and.link.pdf.sh
@@ -9,12 +9,13 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 FILE="$1";
+TMPFILE="tmp-$(basename $FILE)"
 NEWFILE="${FILE/.pdf/-2x1.pdf}"
 
-cp -L "$FILE" /tmp/.  || { echo "Failed to copy $FILE to /tmp; Exiting"; exit 1; } # need to use {\  and \ } [note spaces] and not () (which starts a subshell)
-pdfxup -fw 0 -m 30 -ps 'letter' "/tmp/$FILE" || { echo "Failed to run pdfxup on /tmp/$FILE; Exiting"; exit 1; } #pdfxup.pdf should be in current dir
+cp -L "$FILE" "/tmp/$TMPFILE"  || { echo "Failed to copy $FILE to /tmp/$TMPFILE; Exiting"; exit 1; } # need to use {\  and \ } [note spaces] and not () (which starts a subshell)
+pdfxup -fw 0 -m 30 -ps 'letter' -o pdfxup.pdf "/tmp/$TMPFILE" || { echo "Failed to run pdfxup on /tmp/$TMPFILE; Exiting"; exit 1; } #pdfxup.pdf should be in current dir
  # Note, because I am using an alias for pdfxup, there's an issue with the exit code from pdfxup not being properly passed.
-rm -f "/tmp/$FILE" || { echo "Failed to remove /tmp/$FILE ; Exiting"; exit 1; }
+rm -f "/tmp/$TMPFILE" || { echo "Failed to remove /tmp/$TMPFILE ; Exiting"; exit 1; }
 mv "pdfxup.pdf" "$NEWFILE"  || { echo "Failed to copy pdfxup.pdf to $NEWFILE"; exit 1; }
 
 if [[ "$0" = *.and.link.pdf.sh ]]; then 
