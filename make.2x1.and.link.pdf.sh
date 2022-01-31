cc172d98aa54253d54afc13c8eefe4997a34cebd
Michael Gilchrist <mikeg@utk.edu>
2022-01-31 11:00:58 -0500

    modified make.2x1.and.link.pdf.sh to use optional arguments for pdfxup

--- make.2x1.and.link.pdf.sh
+++ make.2x1.and.link.pdf.sh
@@ -2,11 +2,12 @@
 # make 2x1 version of a paper
 # 
 
+## No longer needed, allow passing of optional arguments
 ## FIrst error check
-if [[ "$#" -ne 1 ]]; then
-    echo "Error in $0:  One argument expected: <FILE.pdf>";
-    exit 1;
-fi
+#if [[ "$#" -ne 1 ]]; then
+#    echo "Error in $0:  One argument expected: <FILE.pdf>";
+#    exit 1;
+#fi
 
 if [[ "$0" = *.4x1.sh ]]; then
     COL=2;
@@ -22,43 +23,65 @@ else  ## 2x1
     FW=0;
     LND=1;
     SUFF="2x1";
-    MAR=5; # was 30
+    MAR=30;
     ISP=0;
 fi
 
     
 FILE="$1";
+echo "FILE = $FILE"
+
+OPTARGS="${@:2}"
+# I thought OPTARGS might have newlines, but it doesn't seem to
+#OPTARGS=${OPTARGS/$'\n'/ /}
+
+echo "OPTARGS = $OPTARGS; length = ${#OPTARGS}"
+
 TMPFILE="tmp-$(basename $FILE)"
 NEWFILE="${FILE/.pdf/-$SUFF.pdf}"
 
-PAGES=$(pdfinfo $FILE | grep "^Pages" | awk '{print $2}')
+## Commented out after add $OPTARGS
+#PAGES=$(pdfinfo $FILE | grep "^Pages" | awk '{print $2}')
+### Truncate if long file
+#if [[ $PAGES -ge 20 ]]; then
+#    PAGES=20
+#fi
 
-#echo "Pages: $PAGES"
-#echo "File: $FILE"
-#echo "New File: $NEWFILE"
+#Use page 2 by default
+PAGES=2
+#"2-3"
+echo "Pages: $PAGES"
+echo "Tmp File: $TMPFILE"
+echo "New File: $NEWFILE"
 
-cp -L "$FILE" "/tmp/$TMPFILE"  || { echo "Failed to copy $FILE to /tmp/$TMPFILE; Exiting"; exit 1; } # need to use {\  and \ } [note spaces] and not () (which starts a subshell)
+cp -Lf "$FILE" "/tmp/$TMPFILE"  || { echo "Failed to copy $FILE to /tmp/$TMPFILE; Exiting"; exit 1; } # need to use {\  and \ } [note spaces] and not () (which starts a subshell)
 
-## Truncate if long file
-if [[ $PAGES -ge 20 ]]; then
-    PAGES=20
-fi
 
 # -V: Verbosity
 # -fw: Frame weight
 # -m: Margins of new document
 # -is: interspace between pages
-~/bin/pdfxup-local -fw "$FW" \
+# Had to remove #  -bb 1-$PAGES \
+
+~/bin/pdfxup-local \
+  -V 2 \
+  -bb "$PAGES" \
+  -fw "$FW" \
   -m "$MAR" \
   -l "$LND" \
   -x "$COL" \
   -y "$ROW" \
-  -ps 'letter' \
-  -V 3 \
-  -is $ISP \
-  -bb 1-$PAGES \
-  -o pdfxup.pdf "/tmp/$TMPFILE" || { echo "Failed to run pdfxup on /tmp/$TMPFILE; Exiting"; exit 1; } #pdfxup.pdf should be in current dir
- # Note, because I am using an alias for pdfxup, there's an issue with the exit code from pdfxup not being properly passed.
+  -is "$ISP" \
+  -ps "letter" \
+  -o pdfxup.pdf $OPTARGS "/tmp/$TMPFILE" || { echo "Failed to run pdfxup on /tmp/$TMPFILE; Exiting"; exit 1; } 
+# NOTE: Don't put quotes around $OPTARGS.  That screws things up, I don't know why.
+#       Perhaps because it is already a string?
+#
+#       pdfxup.pdf should be in current dir
+# 
+#       Because I am using an alias for pdfxup, there's an issue with the exit code from pdfxup not being properly passed.
+
+echo "Removing temporary file /tmp/$TMPFILE"
 rm -f "/tmp/$TMPFILE" || { echo "Failed to remove /tmp/$TMPFILE ; Exiting"; exit 1; }
 mv "pdfxup.pdf" "$NEWFILE"  || { echo "Failed to copy pdfxup.pdf to $NEWFILE"; exit 1; }
 
